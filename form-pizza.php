d95436c14dea8b0c549a972039c55479f71a929d
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-26 09:58:56 -0300

    Create form-pizza.php

---
+++ form-pizza.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiPizzas</title>
+</head>
+<body>
+    <form action="enviar-form-pizza.php" method="post">
+        digite o nome da pizza <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição da pizza <br>
+        <input type="text" name="descricao"> <br>
+        digite o preço da pizza <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem da pizza <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
