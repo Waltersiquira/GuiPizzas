458a0ba18e3a34f617fa4c44d226a647c3ade9ee
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-26 09:53:13 -0300

    Create pagina-pizza.php

---
+++ pagina-pizza.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiPizzas</title>
+</head>
+<body>
+    <?php require_once 'includes/lojas.php' ?>
+    <?php 
+    $id = $_GET['i'] ?? 1;
+    $busca = $l->query("select * from pizza where id = '$id'");
+    if (!$busca){
+        echo 'erro';
+    } else {
+        if ($busca->num_rows == 0){
+            echo 'erro';
+        } else {
+            while ($reg=$busca->fetch_object()){
+                echo "<img src='$reg->imagem' width='200'> <h3>$reg->nome</h3> <h4>$reg->preço</h4> <p>$reg->descrição</p>";
+            }
+        }
+    }
+    ?>
+    <button style="background-color: blue;"><a href="comprar-pizza.php" style="color: white; text-decoration: none;">Comprar</a></button>
+</body>
+</html>
